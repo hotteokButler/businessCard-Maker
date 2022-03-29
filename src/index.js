724c3d378790af5a8eb35acf413640523d5c9773
jisooyoon <ygs12567@gmail.com>
2022-03-29 14:55:39 +0900

    Basic Set Up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.module.css';
+import './reset.css';
 import App from './app.jsx';
 
 ReactDOM.render(
